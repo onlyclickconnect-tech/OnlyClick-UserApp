80a4f61b854708256a31abb44b77160540568104
Amar <onlyaboutcoc@gmail.com>
2025-09-06 19:10:04 +0530

    fixed the sub_category in services section

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import supabase from "../../data/supabaseClient";
 
 const api = axios.create({
-  baseURL: "https://witty-paths-see.loca.lt",
+  baseURL: "https://twenty-bats-boil.loca.lt",
 });
 
 api.interceptors.request.use(async (config) => {
