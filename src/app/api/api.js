3d0bd623237b3803d649edc08d1b8e69c14d4d95
Amar <onlyaboutcoc@gmail.com>
2025-09-11 00:30:49 +0530

    some

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import supabase from "../../data/supabaseClient";
+import supabase from "../../data/supabaseClient.js";
 
 const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
