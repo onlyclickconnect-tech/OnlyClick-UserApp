13800e96b1ef041955bc41359d10f51ba94cfda4
sravanidasari4488 <154404196+sravanidasari4488@users.noreply.github.com>
2025-11-07 21:08:05 +0530

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "Only Click",
     slug: "only-click-user",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/images/mainicon.png",
     scheme: "onlyclick",
